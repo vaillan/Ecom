a60434558798f28ef8b18c5be8df376d891b59de
Valentin Ortiz Santiago <red.line.v.o.s@outlook.com>
2021-05-06 22:23:15 -0500

    Actualizacion1.2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call([
-            MexicoAddressSeeder::class,
             UserSeeder::class,
             EstadosSeeder::class,
             MunicipiosSeeder::class,
